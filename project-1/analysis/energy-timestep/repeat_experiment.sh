59fd2ca42230e2e6fa4d236e68ac9612675a233c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-18 11:05:26 +0100

    Add analysis of total energy

--- project-1/analysis/energy-timestep/repeat_experiment.sh
+++ project-1/analysis/energy-timestep/repeat_experiment.sh
@@ -3,8 +3,10 @@ export OMP_NUM_THREADS=4
 
 for i in $(seq 2 5); do
     echo For i = $i we get dt = $(python -c "print('%f' % 10 ** (-$i))")
+    echo $(python -c "print(__import__('math').ceil(10 / 10 ** (-$i)))")
     ~/lammps/build/lmp \
         -var timestep $(python -c "print('%f' % 10 ** (-$i))") \
         -var filename thermo_$i.log \
+        -var num_steps $(python -c "print(__import__('math').ceil(10 / 10 ** (-$i)))") \
         < energy.conf
 done
