bf2c33c1bda3a2ddaac0686ed411e849c65a9b2e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-04 09:48:02 +0100

    Create new reader using data from dump

---
+++ project-1/read_lammps_dump.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+
+def read_slice(dat_slice):
+    dat_slice = dat_slice.split("\n")[:-1]
+
+    timestep = int(dat_slice[1])
+    num_atoms = int(dat_slice[3])
+    num_dim = dat_slice[4].split().count("pp")
+    bounds = []
+
+    for i in range(num_dim):
+        bound = dat_slice[5 + i].split()
+        bounds.append(tuple(map(lambda x: float(x), bound)))
+
+    headers = dat_slice[5 + num_dim].split()[2:]
+    data_start = 5 + num_dim + 1
+
+    df_slice = pd.read_csv(
+        pd.compat.StringIO("\n".join(dat_slice[data_start:])),
+        sep=r"\s+",
+        index_col=False,
+        names=headers,
+    )
+    df_slice["timestep"] = timestep
+    df_slice = df_slice.sort_values(["timestep", "id"]).reset_index()
+    del df_slice["index"]
+
+    return df_slice, num_atoms, bounds
+
+
+def read_dump(filename):
+    with open(filename, "r") as f:
+        dat = f.read()
+
+    prev_pos = 0
+    current_pos = 0
+
+    df = []
+
+    while current_pos < len(dat):
+        current_pos = dat.find("ITEM: TIMESTEP", prev_pos + 1)
+
+        if current_pos < 0:
+            current_pos = len(dat)
+
+        dat_slice = dat[prev_pos:current_pos]
+        df_slice, num_atoms, bounds = read_slice(dat_slice)
+
+        df.append(df_slice)
+
+        prev_pos = current_pos
+
+    return pd.concat(df), num_atoms, bounds
+
+
+if __name__ == "__main__":
+    df, num_atoms, bounds = read_dump("dump.lammpstrj")
+    print(num_atoms)
+    print(bounds)
+    print(df.head())
+    print(df.tail())
