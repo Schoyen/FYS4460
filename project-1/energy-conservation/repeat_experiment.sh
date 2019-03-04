257840a7b1ca5421177326827c0da49467dbf233
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-03-04 11:01:59 +0100

    Add scripts for energy conservation data

---
+++ project-1/energy-conservation/repeat_experiment.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+export OMP_NUM_THREADS=4
+
+for i in $(seq 2 5); do
+    echo For i = $i we get dt = $(python -c "print('%f' % 10 ** (-$i))")
+    echo $(python -c "print(__import__('math').ceil(10 / 10 ** (-$i)))")
+    mpirun -n 4 lmp \
+        -var timestep $(python -c "print('%f' % 10 ** (-$i))") \
+        -var filename thermo_$i.log \
+        -var num_steps $(python -c "print(__import__('math').ceil(10 / 10 ** (-$i)))") \
+        < energy.conf
+done
